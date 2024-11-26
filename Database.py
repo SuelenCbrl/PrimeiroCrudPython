a30038c3fd81b27e7c4f5f1c1df7e6bf5df1b2e3
SuelenCbrl <suelen.cbrl.sc@gmail.com>
2024-11-26 08:55:34 -0400

    Atualização crud
    Cadastro de produtos

--- Database.py
+++ Database.py
@@ -85,7 +85,81 @@ class Database():
         except Exception as err:
             print(err)
 
+    def insert_produto(self):
+        self.connect()
+        try:
+            args1=("Fichario ","tilibra","A5","Caderno tamnaho A3 tema Harry Potter","R$89,90","A5","21x20","180mg")
 
+            self.cursor.execute('INSERT INTO produto (nome_prod,marca,modelo,descricao,preco,tipo,tamanho,peso) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)',args1)
+            self.conn.commit()
+            print("Produto cadastrado com sucesso!")
+
+        except Exception as err:
+            print(err)
+
+
+    def select_produto(self):
+        self.connect()
+        try:
+            self.cursor.execute("SELECT * FROM produto")
+            produto = self.cursor.fetchall()
+            for item in produto:
+                print(item)
+
+        except Exception as err:
+            print(err)
+
+    def select_produto_by_id(self,id_prod):
+        self.connect()
+        try:
+            self.cursor.execute(f"SELECT * FROM produto WHERE id_prod = {id_prod}")
+            prod = self.cursor.fetchone()
+            return prod
+
+        except Exception as err:
+            print(err)
+
+    def update_produto(self,id_prod):
+        self.connect()
+        try:
+            produto = list(self.select_produto_by_id(id_prod))
+            produto[1]=input("Digite o novo nome:")
+            produto[2]=input("Digite a marca: ")
+            produto[3]=input("Digite o modelo: ")
+            produto[4]=input("Digite o descricao: ")
+            produto[5]=input("Digite o preco: ")
+            produto[6]=input("Digite o tipo: ")
+            produto[7]=input("Digite o tamanho: ")
+            produto[8]=input("Digite o peso: ")
+
+            self.cursor.execute(f"""UPDATE produto SET 
+                                nome_prod = '{produto[1]}',
+                                marca ='{produto[2]}',
+                                modelo = '{produto[3]}',
+                                descricao='{produto[4]}',
+                                preco='{produto[5]}',
+                                tipo='{produto[6]}',
+                                tamanho='{produto[7]}',
+                                peso='{produto[8]}'
+                                WHERE id_prod = {produto[0]}""")
+            self.conn.commit()
+            pro_atualizado = self.select_produto_by_id(produto[0])
+            print(pro_atualizado,"Produto atualizado com sucesso!")
+    
+   
+        except Exception as err:
+            print(err)
+
+
+    def delete_produto(self,id_prod):
+        self.connect()
+        try:
+            self.cursor.execute(f"DELETE FROM produto  WHERE id_prod ={id_prod}")
+            self.conn.commit()
+            print("Produto Deletado com sucesso!")
+        
+        except Exception as err:
+            print(err)
     def close_connection(self):
         if self.conn.is_connected():
             self.cursor.close()
@@ -98,6 +172,11 @@ if __name__=='__main__':
     # db.select_client()
     # db.select_client_by_id(2)
     # db.update_client(2)
-    db.update_client(5)
+    # db.update_client(5)
+    # db.insert_produto()
+    # db.select_produto()
+    # db.select_produto_by_id(1)
+    # db.update_produto(5)
+    # db.delete_produto(4)
     db.close_connection()
     
